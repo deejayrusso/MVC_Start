6261e331fe87f63e45431631e7294a35e5c74b65
deejayrusso <djditucci@gmail.com>
2021-06-08 19:34:26 -0400

    Added EF model

---
+++ MVC_Start/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_EF_Start.Models;
+
+namespace MVC_EF_Start.DataAccess
+{
+  public class ApplicationDbContext : DbContext
+  {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrolment> Enrolments { get; set; }
+    }
+}
